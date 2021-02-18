2da461ac42a34a6c24397fa1b0e80cf4af39605b
Petr D. Novak <petr-novak-donald@protonmail.com>
2021-02-18 12:01:46 +0100

    array converted to list, add print of total tax

--- src/pcgTax/TestProgram.java
+++ src/pcgTax/TestProgram.java
@@ -1,6 +1,7 @@
 package pcgTax;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestProgram {
 
@@ -14,14 +15,32 @@ public class TestProgram {
         var t1 = new Trolleybus();
 
         //pouziti listu
-        //List<Inter> zdanit = new List<Inter>() { h1, f1, m1, c1, t1 };
-        //foreach (Inter item in zdanit) {System.out.println($"{item.GetType().Name} - sazba danne: {item.GetTax()}"); }
-        //pouziti pole
-        Inter[] zdanit = new Inter[] { h1, f1, m1, c1, t1 };
-        for (int i = 0; i < zdanit.length; i++)
-        {
-            System.out.println(zdanit[i].getClass().getName().substring(zdanit[i].getClass().getName().indexOf(".")+1) +"- sazba danne:" + zdanit[i].GetTax());
+        List<Inter> zdanit = new ArrayList<Inter>();
+        zdanit.add(h1);
+        zdanit.add(f1);
+        zdanit.add(m1);
+        zdanit.add(c1);
+        zdanit.add(t1);
+        
+       double celkDan=0;
+        
+        for(Inter item : zdanit) {
+        	
+        	System.out.println(item.getClass().getName().substring(item.getClass().getName().indexOf(".")+1) +"- sazba danne:" + item.GetTax());
+        	celkDan+=item.GetTax(); 
         }
+        
+        System.out.println("Celkova dan je:"+ celkDan);
+        
+        
+        
+        
+        //pouziti pole
+       // Inter[] zdanit = new Inter[] { h1, f1, m1, c1, t1 };
+        //for (int i = 0; i < zdanit.length; i++)
+        //{
+          //  System.out.println(zdanit[i].getClass().getName().substring(zdanit[i].getClass().getName().indexOf(".")+1) +"- sazba danne:" + zdanit[i].GetTax());
+        //}
        
 		
 
